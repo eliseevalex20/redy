af4d786f9bc6771a251a6d703d5b254363611fda
eliseevalex20 <64779288+eliseevalex20@users.noreply.github.com>
2020-07-31 22:59:25 +0300

    cool-commit

--- js/min_js.js
+++ js/min_js.js
@@ -276,38 +276,4 @@ $(function(){
     })
 });
 
-$(function(){
 
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1
-    });
-    $('.multiple-items_Personal').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-
-    $('.multiple-items_mob').slick({
-        responsive: [
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
-})
